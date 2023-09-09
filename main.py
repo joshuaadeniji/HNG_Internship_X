f3b5fbf90653a1eff26f688bc19f5d46715affdc
Joshua Adeniji <joshua.o.adeniji@gmail.com>
2023-09-09 23:06:34 +0100

    update main.py
    commit updates to main.py

--- main.py
+++ main.py
@@ -3,22 +3,23 @@ import requests
 from datetime import datetime
 
 app = Flask(__name__)
-repo_url = 'https://github.com/joshuaadeniji/hngx_zuri/blob/main/README.md'
-response = requests.get(repo_url)
+github_file_url = 'https://github.com/joshuaadeniji/HNG_Internship_X/blob/main/main.py'
+response = requests.get(github_file_url)
 today = datetime.now()
 
 @app.route('/user/', methods=['GET'])
+
 def user_page():
 
-    slack_username = str(request.args.get('user'))  #/username/?user='Joshua_Adeniji'
-    track = str(request.args.get('track'))  #/track/?track=backend
+    slack_username = str(request.args.get('user'))  # user=Joshua Adeniji
+    track = str(request.args.get('track'))  # track=backend
 
     data_set = {"slack_name": f"{slack_username}",
     "current_day": today.strftime('%A'),
     "utc_time": today.utcnow(),
     "track": f"{track}",
-    "github_file_url": "https://github.com/joshuaadeniji/hngx_zuri/blob/main/README.md",
-    "github_repo_url": "https://github.com/joshuaadeniji/hngx_zuri",
+    "github_file_url": "https://github.com/joshuaadeniji/HNG_Internship_X/blob/main/main.py",
+    "github_repo_url": "https://github.com/joshuaadeniji/HNG_Internship_X",
     "status_code": response.status_code}
     
     json_dump = json.dumps(data_set)
