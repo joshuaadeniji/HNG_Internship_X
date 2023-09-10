50f38ff5461f0dcb263ca1bc7bba49f265cbe638
Joshua Adeniji <joshua.o.adeniji@gmail.com>
2023-09-10 02:14:03 +0100

    Update main.py
    final update

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import *
+from flask import Flask, json, request
 import requests
 from datetime import datetime
 
 app = Flask(__name__)
-github_file_url = 'https://github.com/joshuaadeniji/HNG_Internship_X/blob/main/main.py'
-response = requests.get(github_file_url)
+file_url = 'https://joshuaadeniji.pythonanywhere.com/user/?user=Joshua%20Adeniji&track=backend/'
+response = requests.get(file_url)
 today = datetime.now()
 
 @app.route('/user/', methods=['GET'])
@@ -21,10 +21,10 @@ def user_page():
     "github_file_url": "https://github.com/joshuaadeniji/HNG_Internship_X/blob/main/main.py",
     "github_repo_url": "https://github.com/joshuaadeniji/HNG_Internship_X",
     "status_code": response.status_code}
-    
+
     json_dump = json.dumps(data_set)
 
     return json_dump
 
 if __name__ == '__main__':
-    app.run(port=7777)
+    app.run(port=7000)
