b9a6adb10075df6246ab2ff30843b0a1252fcd4b
Joshua Adeniji <joshua.o.adeniji@gmail.com>
2023-09-11 14:57:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ def user_page():
 
     data_set = {"slack_name": f"{slack_name}",
     "current_day": today.strftime('%A'),
-    "utc_time": today.utcnow(),
+    "utc_time": today.strftime('%Y-%m-%dT%H:%M:%SZ'),
     "track": f"{track}",
     "github_file_url": "https://github.com/joshuaadeniji/HNG_Internship_X/blob/main/main.py",
     "github_repo_url": "https://github.com/joshuaadeniji/HNG_Internship_X",
-    "status_code": response.status_code}
+    "status_code": int(response.status_code)}
 
     json_dump = json.dumps(data_set)
 
@@ -29,3 +29,4 @@ def user_page():
 
 if __name__ == '__main__':
     app.run(port=7070)
+
