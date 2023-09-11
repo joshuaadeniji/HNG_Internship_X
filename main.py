968e1c0fc9760f78e7bea3706837bd51bc68c45d
Joshua Adeniji <joshua.o.adeniji@gmail.com>
2023-09-11 18:06:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, json, request
+from flask import Flask, request, jsonify
+# import json
 import requests
 from datetime import datetime
 
@@ -15,18 +16,17 @@ def user_page():
     slack_name = str(request.args.get('slack_name'))  # slack_name=Josh
     track = str(request.args.get('track'))  # track=backend
 
-    data_set = {"slack_name": f"{slack_name}",
+    data_set = {
+    "slack_name": f"{slack_name}",
     "current_day": today.strftime('%A'),
-    "utc_time": today.strftime('%Y-%m-%dT%H:%M:%SZ'),
+    "utc_time": datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
     "track": f"{track}",
     "github_file_url": "https://github.com/joshuaadeniji/HNG_Internship_X/blob/main/main.py",
     "github_repo_url": "https://github.com/joshuaadeniji/HNG_Internship_X",
-    "status_code": int(response.status_code)}
+    "status_code": int(response.status_code)
+    }
 
-    json_dump = json.dumps(data_set)
-
-    return json_dump
+    return jsonify(data_set)
 
 if __name__ == '__main__':
     app.run(port=7070)
-
